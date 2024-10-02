1c940895e8efa790422c194fc1d2762bdc1f999a
adan <adanabdullahi343@gmail.com>
2024-10-02 09:35:24 +0300

    lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+  }
+  function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    return blocks * 264;
+  }
+  function distanceTravelledInFeet(start, destination) {
+    const blocksTravelled = Math.abs(destination - start);
+    return blocksTravelled * 264;
+  }
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0; // Free for the first 400 feet
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02; // 2 cents per foot after the first 400 feet
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25; // Flat fare of $25 for distance over 2000 feet and up to 2500 feet
+    } else {
+      return 'cannot travel that far'; // For distances over 2500 feet
+    }
+  }
+  
